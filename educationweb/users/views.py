f413b2feb48713ce51bc2213451294b5145cc575
izaldal <izaldal@ac.sce.ac.il>
2021-04-04 16:17:25 +0300

    BSPM2021T12-68 handel the user input and save it in the data base

--- educationweb/users/views.py
+++ educationweb/users/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from .auth import auth
+from .forms import *
 # Create your views here.
+
+def parent_register(req):
+    if req.method == 'POST':
+        form=ParentRegisterForm(req.POST)
+        if form.is_valid():
+            au=auth()
+            parent=form.save(commit=False)
+            au.parent_register(parent)
+            username=form.cleaned_data.get('username')
+            messages.success(req,f'Your account has been created! you are now able to login ')
+            return render(req,'users/home.html')
+    else:
+
+        form=ParentRegisterForm()
+    return render(req,'users/register.html',{'form':form})
