d9dbb24ed9e815348509463e128ba382fe816fff
izaldal <izaldal@ac.sce.ac.il>
2021-04-04 16:21:48 +0300

    django files

--- educationweb/educationweb/settings.py
+++ educationweb/educationweb/settings.py
@@ -29,7 +29,6 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -37,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users'
+    'users',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -119,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK='bootstrap4'
+
+LOGIN_URL='login'
+
+LOGIN_REDIRECT_URL = 'home'
