048acce1f314de7080478256e7e146f2da7f6bae
omar hmdea <user.email=omarhm@ac.sce.ac.il>
2021-04-04 19:02:35 +0300

    BSPM2021T12-208 connect the login page to home page

--- educationweb/users/views.py
+++ educationweb/users/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .auth import auth
 from .forms import *
+from django.contrib import messages
 # Create your views here.
 
 def parent_register(req):
@@ -12,8 +13,27 @@ def parent_register(req):
             au.parent_register(parent)
             username=form.cleaned_data.get('username')
             messages.success(req,f'Your account has been created! you are now able to login ')
-            return render(req,'users/login.html')
+            return redirect("login")
     else:
 
         form=ParentRegisterForm()
     return render(req,'users/register.html',{'form':form})
+
+def teacher_register(req):
+    if req.method == 'POST':
+        form=TeacherRegisterForm(req.POST)
+        if form.is_valid():
+            au=auth()
+            teacher=form.save(commit=False)
+            au.teacher_register(teacher)
+            username=form.cleaned_data.get('username')
+            messages.success(req,f'Your account has been created! you are now able to login ')
+            return redirect("login")
+    else:
+
+        form=TeacherRegisterForm()
+    return render(req,'users/register_teacher.html',{'form':form})
+
+
+def home(req):
+    return render(req,'users/home.html')
