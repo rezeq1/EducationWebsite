775eb42cd1bfe8db939c6077bc4d47de9ca58a3b
izaldal <izaldal@ac.sce.ac.il>
2021-04-04 16:18:40 +0300

    BSPM2021T12-185 register method unit test

--- educationweb/users/tests.py
+++ educationweb/users/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
-
+from .models import Parent
+from .auth import *
 # Create your tests here.
+class AuthTestCase(TestCase):
+   
+
+    def test_parent_register(self):
+        a=auth()
+        p=Parent(password='123456',username='username',first_name='user',last_name='last',email='email@email')
+        a.parent_register(p)
+        pregusterd=Parent.objects.filter(username='username').first()
+        p.delete()
+        self.assertIsNotNone(pregusterd)
