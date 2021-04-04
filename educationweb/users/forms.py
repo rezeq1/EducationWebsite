b461a3c857a7029de85f6361eee653b277835db8
izaldal <izaldal@ac.sce.ac.il>
2021-04-04 16:14:56 +0300

    BSPM2021T12-67 form to check if the paren is exist and input fields

---
+++ educationweb/users/forms.py
@@ -0,0 +1,18 @@
+from django import forms 
+from django.contrib.auth.models import User 
+from django.contrib.auth.forms import UserCreationForm 
+from .models import * 
+
+class ParentRegisterForm(UserCreationForm): 
+    first_name = forms.CharField(max_length=30, required=True) 
+    last_name = forms.CharField(max_length=30, required=True) 
+    email = forms.EmailField(max_length=254,required=True ) 
+    username = forms.CharField(max_length=30, required=True)
+    password1 = forms.PasswordInput()
+    password2 = forms.PasswordInput() 
+    class Meta: 
+        model = Parent 
+        fields = [ 'username',
+         'first_name',
+          'last_name', 'email', 
+          'password1', 'password2', ] 
