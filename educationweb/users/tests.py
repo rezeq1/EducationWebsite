7c9a7b5da3868ee5f96941c573f8ba86169e30a8
izaldal <izaldal@ac.sce.ac.il>
2021-04-07 18:31:18 +0300

    fix pipeline and docker

--- educationweb/users/tests.py
+++ educationweb/users/tests.py
@@ -19,4 +19,16 @@ class AuthTestCase(TestCase):
         a.teacher_register(t)
         Tregisterd=Teacher.objects.filter(username='username').first()
         t.delete()
-        self.assertIsNone(Tregisterd)
+        self.assertIsNotNone(Tregisterd)
+    
+    def test_2(self):
+        self.assertTrue(True)
+    
+    def test_3(self):
+        self.assertTrue(False)
+    
+    def test_4(self):
+        self.assertTrue(True)
+
+    def test_5(self):
+        self.assertTrue(True)
