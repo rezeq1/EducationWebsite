c769b9245d52afa5022d3ec2ec3d455777d7eab3
rezeq1 <rezeqab@ac.sce.ac.il>
2021-05-01 16:39:07 +0300

    BSPM2021T12-227 unit test for adding questions

--- educationweb/kindergarten/tests/test_kindergarten.py
+++ educationweb/kindergarten/tests/test_kindergarten.py
@@ -24,4 +24,18 @@ class gartenTestCase(TestCase):
         g=garten()
         HW=g.addHomeWork("Test",10,Kk)
         self.assertIsNotNone(HW)
+    
+            
+    def test_add_Question(self):
+        t=Teacher(password='123456',username='username',first_name='user',last_name='last',email='email@email')
+        t.save()
+        Kk=Kindergarten(name='test',seatLimit=20,myTeacher=t)
+        Kk.save()
+        g=garten()
+        HW=g.addHomeWork("Test",10,Kk)
+        q=Question()
+        q.question='test'
+        q.save()
+        g.addQuestion(q,HW)
+        self.assertIsNotNone(q.homeWork)
         
